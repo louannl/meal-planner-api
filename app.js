6efef63bd85de5b35d4bd133e47af101f04a279f
louannl <louann_loizou@hotmail.com>
2021-04-13 16:21:26 +0100

    added db file and noted routes to be made

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const port = 5000;
+const cors = require('cors');
+const pool = require('./db');
 
-app.listen(5000, () => {
-  console.log('server has started on port 5000');
+app.use(cors());
+app.use(express.json()); //used to parse json bodies, don't need bodyParser for this.
+
+//GET MEAL/:id
+//POST MEAL
+//PUT MEAL/:id
+//DELETE MEAL/:id
+
+//GET ALL MEALS
+
+//GET ALL INGREDIENTS --not sure if needed as can be requested with all meals
+
+app.listen(port, () => {
+  console.log(`server has started on port ${port}`);
 });
