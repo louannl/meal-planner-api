33c68273de7fbbd53dfc48d61eb397e6a99be5bd
louannl <louann_loizou@hotmail.com>
2021-04-15 13:29:48 +0100

    deleted old meal code which wasn't in use

--- routes/meals.js
+++ routes/meals.js
@@ -10,44 +10,11 @@ handler.getAll(router, 'meals');
 
 //GET UNITS
 //POST MEAL
-const createMeal = (req, res) => {
-  const { name, dayId } = req.body;
+// add day_meals record
+// add ingredients to DB, check if ingredient already exists
+// add ingredients to meal via meal_ingredients
 
-  pool.query(
-    'INSERT INTO meals (name) VALUES ($1)',
-    [name],
-    (error, result) => {
-      if (error) {
-        throw error;
-      }
-      const mealId = result.insertId;
-      // add day_meals record
-      pool.query(
-        'INSERT INTO day_meals (meal_id, day_id) VALUES ($1, $2)',
-        [mealId, dayId],
-        (error, result) => {
-          if (error) {
-            throw error;
-          }
-        }
-      );
-      // add ingredients to DB, check if ingredient already exists
-
-      // add ingredients to meal via meal_ingredients
-
-      res.status(201).send(`Meal added with ID: ${result.insertId}`);
-    }
-  );
-};
 //PUT MEAL/:id
 //DELETE MEAL/:id
 //GET ALL INGREDIENTS
 //GET ALL MEALS
-const getMeals = (req, res) => {
-  pool.query('SELECT * FROM meals', (err, results) => {
-    if (error) {
-      throw error;
-    }
-    res.status(200).json(results.rows);
-  });
-};
