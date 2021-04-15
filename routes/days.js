33b3231aed3993c3f0140c83012fb0e8448cb3a1
louannl <louann_loizou@hotmail.com>
2021-04-15 12:03:16 +0100

    added getall and get handler with days

--- routes/days.js
+++ routes/days.js
@@ -1,1 +1,9 @@
-//GET Days
+const Router = require('express-promise-router');
+const handler = require('./handler');
+
+const router = new Router();
+module.exports = router;
+
+//GET ALL DAYS
+handler.getAll(router, 'days');
+//GET DAY
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const meals = require('./meals');
 const tags = require('./tags');
+const days = require('./days');
 //const ingredients = require('/ingredients');
 
 module.exports = (app) => {
   app.use('/meals', meals);
   app.use('/tags', tags);
+  app.use('/days', days);
   //app.use('/ingredients', ingredients);
 };
