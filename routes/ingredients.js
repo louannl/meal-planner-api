907b439a761a91af4c530485632517d87f2098a9
louannl <louann_loizou@hotmail.com>
2021-04-15 21:18:49 +0100

    added update method for ingredients

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -12,46 +12,97 @@ handler.getAll(router, 'ingredients');
 handler.getOne(router, 'ingredients');
 handler.deleteOne(router, 'ingredients');
 handler.deleteAll(router, 'ingredients');
-//ADD INGREDIENT
-router.post(
-  '/',
-  validate(
-    checkSchema({
-      name: {
-        errorMessage: 'Name cannot be empty',
-        notEmpty: true,
-        in: 'body',
-      },
-      unitTypeId: {
-        errorMessage: 'Unit ID is not valid',
-        notEmpty: true,
-        in: 'body',
-        isInt: true,
-        //sanitizer
-        toInt: true,
-      },
-    })
-  ),
-  async (req, res) => {
-    const { name, unitTypeId } = req.body;
 
-    if (await checks.nameExists(name, 'ingredients')) {
-      return res.sendStatus(200);
+router
+  .post(
+    '/',
+    validate(
+      checkSchema({
+        name: {
+          errorMessage: 'Name cannot be empty',
+          notEmpty: true,
+          in: 'body',
+        },
+        unitTypeId: {
+          errorMessage: 'Unit ID is not valid',
+          notEmpty: true,
+          in: 'body',
+          isInt: true,
+          //sanitizer
+          toInt: true,
+        },
+      })
+    ),
+    async (req, res) => {
+      const { name, unitTypeId } = req.body;
+
+      if (await checks.nameExists(name, 'ingredients')) {
+        return res.sendStatus(200);
+      }
+
+      const {
+        rowCount,
+      } = await db.query(
+        'INSERT INTO ingredients (name, unit_type_id) VALUES ($1, $2)',
+        [name, unitTypeId]
+      );
+
+      if (rowCount === 1) {
+        return res.sendStatus(201);
+      }
+
+      res.sendStatus(500);
     }
+  )
+  .put(
+    '/:id',
+    validate(
+      checkSchema({
+        name: {
+          errorMessage: 'Name is not valid',
+          notEmpty: true,
+          in: 'body',
+        },
+        id: {
+          errorMessage: 'ID is not valid',
+          notEmpty: true,
+          in: 'params',
+          isInt: true,
+          toInt: true,
+        },
+        unitTypeId: {
+          errorMessage: 'Unit ID is not valid',
+          notEmpty: true,
+          in: 'body',
+          isInt: true,
+          //sanitizer
+          toInt: true,
+        },
+      })
+    ),
+    async (req, res) => {
+      const { id } = req.params;
+      const { name, unitTypeId } = req.body;
 
-    const {
-      rowCount,
-    } = await db.query(
-      'INSERT INTO ingredients (name, unit_type_id) VALUES ($1, $2)',
-      [name, unitTypeId]
-    );
+      if (!(await checks.idExists(id, 'ingredients'))) {
+        return res.sendStatus(200);
+      }
 
-    if (rowCount === 1) {
-      return res.sendStatus(201);
+      if (await checks.nameExists(name, 'ingredients')) {
+        return res.sendStatus(200);
+      }
+
+      const {
+        rowCount,
+      } = await db.query(
+        `UPDATE ingredients SET name = ($1), unit_type_id = ($2) WHERE id = ($3)`,
+        [name, unitTypeId, id]
+      );
+
+      if (rowCount === 1) {
+        return res.sendStatus(200);
+      }
+
+      res.sendStatus(500);
     }
-
-    res.sendStatus(500);
-  }
-);
-
-//EDIT INGREDIENT
+  );
