611e5e6e750709852dcb03d71bef46552e1221ba
louannl <louann_loizou@hotmail.com>
2021-04-15 12:31:11 +0100

    added post and delete handlers

--- routes/days.js
+++ routes/days.js
@@ -4,6 +4,5 @@ const handler = require('./handler');
 const router = new Router();
 module.exports = router;
 
-//GET ALL DAYS
 handler.getAll(router, 'days');
-//GET DAY
+handler.getOne(router, 'days');
--- routes/handler.js
+++ routes/handler.js
@@ -2,6 +2,21 @@ const { checkSchema } = require('express-validator');
 const validate = require('../validation');
 const db = require('../db');
 
+const tagNameExists = async (name, table) => {
+  const { rowCount } = await db.query(
+    `SELECT id FROM ${table} WHERE name = $1`,
+    [name]
+  );
+  return rowCount > 0;
+};
+
+const tagIdExists = async (id, table) => {
+  const { rowCount } = await db.query(`SELECT id FROM ${table} WHERE id = $1`, [
+    id,
+  ]);
+  return rowCount > 0;
+};
+
 exports.getAll = (router, table) => {
   router.get('/', async (req, res) => {
     const { rows, rowCount } = await db.query(`SELECT * FROM ${table}`);
@@ -43,4 +58,70 @@ exports.getOne = (router, table) => {
 
       res.send(rows[0]);
     };
+};
+
+exports.createOne = (router, table) => {
+  router.post(
+    '/',
+    validate(
+      checkSchema({
+        name: {
+          errorMessage: 'Name cannot be empty',
+          notEmpty: true,
+          in: 'body',
+        },
+      })
+    ),
+    async (req, res) => {
+      const { name } = req.body;
+      if (await tagNameExists(name, table)) {
+        return res.sendStatus(200);
+      }
+
+      const {
+        rowCount,
+      } = await db.query(`INSERT INTO ${table} (name) VALUES ($1)`, [name]);
+
+      if (rowCount === 1) {
+        return res.sendStatus(201);
+      }
+
+      res.sendStatus(500);
+    }
+  );
+};
+
+exports.deleteOne = (router, table) => {
+  router.delete(
+    '/:id',
+    validate(
+      checkSchema({
+        id: {
+          errorMessage: 'ID is not valid',
+          notEmpty: true,
+          in: 'params',
+          isInt: true,
+          //sanitizer
+          toInt: true,
+        },
+      })
+    ),
+    async (req, res) => {
+      const { id } = req.params;
+
+      if (!(await tagIdExists(id, table))) {
+        return res.sendStatus(200);
+      }
+
+      const {
+        rowCount,
+      } = await db.query(`DELETE FROM ${table} WHERE id = ($1)`, [id]);
+
+      if (rowCount === 1) {
+        return res.sendStatus(200);
+      }
+
+      res.sendStatus(500);
+    }
+  );
 };
