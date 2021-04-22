64dc8311aa94e1fe8cb23c1cf7113efe152218c7
louannl <louann_loizou@hotmail.com>
2021-04-22 10:16:12 +0100

    added validation to create meals on router

--- routes/mealRoutes.js
+++ routes/mealRoutes.js
@@ -1,6 +1,8 @@
 import Router from 'express-promise-router';
 import { getOne, getAll } from './handler.js';
 import { createMeal } from '../domain/meals.js';
+import validate from '../validation/index.js';
+import { checkSchema } from 'express-validator';
 
 const router = new Router();
 export default router;
@@ -14,7 +16,47 @@ router.get('/');
 
 //('SELECT * from meals_days WHERE day_id = ($1)'), [dayId] )
 
-router.post('/', async (req, res) => {
-  const { dayId, mealName, mealTags, ingredients } = req.body;
-  return await createMeal(dayId, mealName, mealTags, ingredients);
-});
+router.post(
+  '/',
+  validate(
+    checkSchema({
+      mealName: {
+        errorMessage: 'Name cannot be empty',
+        notEmpty: true,
+        in: 'body',
+      },
+      dayId: {
+        errorMessage: 'Day Id is not valid',
+        notEmpty: true,
+        in: 'body',
+        isInt: true,
+        toInt: true,
+      },
+      mealTags: {
+        errorMessage: 'MealTag must be an array',
+        isArray: true,
+        in: 'body',
+      },
+      ingredients: {
+        isObject: true,
+      },
+      'ingredients.*.name': {
+        notEmpty: true,
+      },
+      'ingredients.*.amount': {
+        notEmpty: true,
+        isInt: true,
+        toInt: true,
+      },
+      'ingredients.*.unitType': {
+        notEmpty: true,
+        isInt: true,
+        toInt: true,
+      },
+    })
+  ),
+  async (req, res) => {
+    const { dayId, mealName, mealTags, ingredients } = req.body;
+    return await createMeal(dayId, mealName, mealTags, ingredients);
+  }
+);
