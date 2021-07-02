05ebe7400db830ad4af15d5bab637823056a6fe6
louannl <louann_loizou@hotmail.com>
2021-07-02 14:25:48 +0100

    added test for getting day by id, then added route

--- express/routes/days.js
+++ express/routes/days.js
@@ -7,9 +7,28 @@ export default router;
 
 router.get('/', async (req, res) => {
   const days = await sequelize.models.Day.findAll({
-    attributes: ['id', 'name'],
+    attributes: { exclude: ['createdAt', 'updatedAt'] },
   });
   res.status(200).json(days);
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const day = await sequelize.models.Day.findOne({
+      where: { id: id },
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
+    });
+
+    if (day) {
+      return res.status(200).json({ day });
+    }
+
+    return res.status(404).send('Day with the specified ID does not exist');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
 // export const getAll = async (req, res) => {
--- tests/days.test.js
+++ tests/days.test.js
@@ -5,5 +5,17 @@ describe('Day route', () => {
   it('should show all days', async () => {
     const res = await request(app).get('/days');
     expect(res.statusCode).toEqual(200);
+    res.body.forEach((day) => {
+      expect(day).toHaveProperty('name');
+      expect(day).toHaveProperty('id');
+    });
+  });
+
+  it('should show first day', async () => {
+    const res = await request(app).get('/days/1');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('day');
+    expect(res.body.day).toHaveProperty('name');
+    expect(res.body.day).toHaveProperty('id');
   });
 });
