778c17974d58db64b3b93a3dc5792143da9f3d14
louannl <louann_loizou@hotmail.com>
2021-10-02 12:28:50 +0100

    renamed and added missing route to file

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,9 +1,11 @@
+import tags from './tags.js';
 import days from './days.js';
 import unitTypes from './unitTypes.js';
 import ingredients from './ingredients.js';
 import meals from './meals.js';
 
 const routes = (app) => {
+  app.use('/tags', tags);
   app.use('/days', days);
   app.use('/unit-types', unitTypes);
   app.use('/ingredients', ingredients);
